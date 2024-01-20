649ef60b08d09c1d5ed637ae9d48aba95db4a882
pandasweta <130973588+pandasweta@users.noreply.github.com>
2024-01-20 21:11:36 +0100

    Modified the date, time and other weather details through API.

--- src/index.js
+++ src/index.js
@@ -8,9 +8,25 @@ function showTemperature(response) {
     // console.log(response.data.location.name);
     let tempC = document.querySelector("#city-temp");
     tempC.innerHTML = Math.round(response.data.current.temp_c);
+
     let cityName = document.querySelector("#city");
     cityName.innerHTML = response.data.location.name;
 
+    let description = document.querySelector("#desc");
+    description.innerHTML = `${response.data.current.condition.text}`;
+
+    let humidity = document.querySelector("#weather-app-city-humidity");
+    humidity.innerHTML = `${response.data.current.humidity}%`;
+    let windSpeed = document.querySelector("#weather-app-city-wind");
+    windSpeed.innerHTML = `${response.data.current.wind_kph}km/h`;
+
+    let currentTime = document.querySelector("#time");
+    let date = new Date(response.data.location.localtime_epoch * 1000);
+    currentTime.innerHTML = showTimeDate(date);
+
+
+
+    // console.log(response.data.current.condition.text);
 }
 
 let searchformElement = document.querySelector("#search-form");
@@ -25,6 +41,35 @@ function searchCity(city) {
     axios.get(apiURL).then(showTemperature)
 }
 
+// to show the current time and date
+function showTimeDate(date) {
+    let now = new Date();
+    let current_day = now.getDay();
+    let current_hours = now.getHours();
+    let current_min = now.getMinutes();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = days[current_day];
+
+    if (current_hours < 10) {
+        current_hours = `0${current_hours}`;
+    }
+
+    if (current_min < 10) {
+        current_min = `0${current_min}`;
+    }
+    return `${day} ${current_hours}:${current_min}`;
+    // console.log(today_time);
+    // let today = document.querySelector("p");
+    // today.innerHTML = `${today_time}`;
+}
 searchCity("Pune");
 
 
