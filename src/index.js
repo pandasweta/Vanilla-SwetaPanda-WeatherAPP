5da2fa1408bc95741f1a00491d691710beebb672
pandasweta <130973588+pandasweta@users.noreply.github.com>
2024-01-20 17:23:13 +0100

    Added the search function with API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 function searchformCity(event) {
     event.preventDefault(event);
     let searchInput = document.querySelector("#search-form-input");
-    // console.log(searchInput);
+    searchCity(searchInput.value);
+}
+
+function showTemperature(response) {
+    // console.log(response.data.location.name);
+    let tempC = document.querySelector("#city-temp");
+    tempC.innerHTML = Math.round(response.data.current.temp_c);
     let cityName = document.querySelector("#city");
-    cityName.innerHTML = searchInput.value;
+    cityName.innerHTML = response.data.location.name;
+
 }
 
 let searchformElement = document.querySelector("#search-form");
-// console.log(searchformElement);
 searchformElement.addEventListener("submit", searchformCity)
+
+// to show the seacrch-city and temeperature in realtime through API call
+
+function searchCity(city) {
+    let apiKey = "147a5d4e22274503a0c71826230610";
+    let apiURL = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+    console.log(apiURL);
+    axios.get(apiURL).then(showTemperature)
+}
+
+searchCity("Pune");
+
+
